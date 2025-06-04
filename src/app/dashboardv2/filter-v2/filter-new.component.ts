20dd9c1a75a4da23bd5ab674b587f8590b817731
prabasak23 <pratik.basak@publicissapient.com>
2025-06-05 01:45:24 +0530

    payload data, multiselect sprints data dropdown added

--- src/app/dashboardv2/filter-v2/filter-new.component.ts
+++ src/app/dashboardv2/filter-v2/filter-new.component.ts
@@ -1041,6 +1041,7 @@ export class FilterNewComponent implements OnInit, OnDestroy {
           x['parentId']?.includes(event[0].nodeId) &&
           x['sprintState']?.toLowerCase() == 'closed',
       );
+      this.service.setCurrentProjectSprints(currentProjectSprints);
       if (currentProjectSprints?.length) {
         currentProjectSprints.sort(
           (a, b) =>
