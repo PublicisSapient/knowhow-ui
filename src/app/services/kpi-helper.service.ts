6c96ef8c3f7cbd4ad9a431304365d07c7d48ee3b
RajaKurru <raja.kurru@publicissapient.com>
2025-06-17 20:01:16 +0530

    rally issue update

--- src/app/services/kpi-helper.service.ts
+++ src/app/services/kpi-helper.service.ts
@@ -48,7 +48,8 @@ export class KpiHelperService {
     const dataGroup1 = inputData.dataGroup?.dataGroup1;
     const issueData = inputData.issueData;
     const categoryGroup = inputData.categoryData?.categoryGroup;
-    let excludeCatForTotal = categoryGroup?.filter((cat) => cat.categoryValue === 'NA')
+    let excludeCatForTotal = categoryGroup
+      ?.filter((cat) => cat.categoryValue === 'NA')
       .map((cat) => cat.categoryName);
     let selectedDataGroup;
     let unit;
