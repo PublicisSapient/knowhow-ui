f2102dc93c15c3454eefc8b266141351dacea7de
brahmanand1 <bnm.amethi@gmail.com>
2025-06-18 05:56:05 -0300

    put console log for testing

--- src/app/config/project-config/field-mapping/field-mapping.component.ts
+++ src/app/config/project-config/field-mapping/field-mapping.component.ts
@@ -75,6 +75,7 @@ export class FieldMappingComponent implements OnInit {
         this.selectedConfig,
       );
     } else {
+      console.log('field-mapping-getKPIFieldMappingRelationships() - 78');
       this.router.navigate(['./dashboard/Config/ProjectList']);
     }
     this.kpiId =
@@ -86,6 +87,7 @@ export class FieldMappingComponent implements OnInit {
           (tool) => tool.toolName === 'Jira' || tool.toolName === 'Azure',
         );
       if (!this.selectedToolConfig || !this.selectedToolConfig.length) {
+        console.log('firld-mapping-getKPIFieldMappingRelationships() - 90');
         this.router.navigate(['./dashboard/Config/ProjectList']);
       } else {
         this.getDropdownData();
@@ -128,6 +130,9 @@ export class FieldMappingComponent implements OnInit {
           }
         });
     } else {
+      console.log(
+        'fied-mapping getKPIFieldMappingRelationships() - No project selected',
+      );
       this.router.navigate(['./dashboard/Config/ProjectList']);
     }
   }
