8d354dc2e88bf89f7c8bec0f98902faf459f9573
Vlad Popovici <vlapopov@publicisgroupe.net>
2025-08-21 09:23:50 +0300

    added field mapping improvement

---
+++ src/app/shared-module/conditional-input-v2/conditional-input-v2.component.ts
@@ -0,0 +1,87 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-conditional-input-v2',
+  templateUrl: './conditional-input-v2.component.html',
+  styleUrls: ['./conditional-input-v2.component.css'],
+})
+export class ConditionalInputV2Component implements OnChanges {
+  @Input() id;
+  @Input() fieldConfig;
+  @Input() valueObj;
+  @Output() conditionalInputChange = new EventEmitter();
+  finalValue = [];
+  templateData = [];
+  templateLabels = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.fieldConfig);
+    if (changes.valueObj && this.valueObj.length) {
+      this.templateLabels = this.templateLabelToLowercase(
+        this.valueObj.map((val) => val.labelValue),
+      );
+      this.templateData = this.fieldConfig.options.filter((opt) =>
+        this.templateLabels.includes(opt.labelValue),
+      );
+      this.finalValue = [...this.templateData];
+      this.valueObj.forEach((element) => {
+        const opt = this.fieldConfig.options.filter(
+          (opt) => opt.labelValue === element.labelValue.toLowerCase(),
+        )[0];
+        if (opt) {
+          opt['countValue'] = element.countValue;
+        }
+      });
+    }
+  }
+
+  templateLabelToLowercase = (arr: []) =>
+    arr.map((val: any) => val.toLowerCase());
+
+  setValue(event) {
+    this.templateLabels = this.templateLabelToLowercase(
+      event.value.map((val) => val.labelValue),
+    );
+    this.templateData = this.fieldConfig.options.filter((opt) =>
+      this.templateLabels.includes(opt.labelValue),
+    );
+    const selectedOption = this.templateData.filter(
+      (opt) => opt.labelValue === event.itemValue.labelValue,
+    )[0];
+    if (selectedOption) {
+      selectedOption['countValue'] = selectedOption['minValue'];
+    }
+    this.setOutput();
+  }
+
+  setCounter(event, option) {
+    if (
+      !this.templateData.filter((opt) => opt.labelValue === option.labelValue)
+        .length
+    ) {
+      const newOption = JSON.parse(JSON.stringify(option));
+      newOption.countValue = event.value;
+      this.templateData.push(newOption);
+    } else {
+      this.templateData.filter(
+        (opt) => opt.labelValue === option.labelValue,
+      )[0].countValue = event.value;
+    }
+    this.setOutput();
+  }
+
+  removeFocus(event) {
+    event.target.blur();
+  }
+
+  setOutput() {
+    this.conditionalInputChange.emit(this.finalValue);
+  }
+}
